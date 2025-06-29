b21d8aed43ca198b333a146ee16cc7521832a72e
hj <2365331988@qq.com>
2025-06-29 23:07:25 +0800

    feat: 视频 Button-02

---
+++ packages/components/Icon/Icon.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { IconProps } from "./types";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { omit } from "lodash-es";
+import { computed } from "vue";
+
+defineOptions({
+  name: "ErIcon",
+  inheritAttrs: false, // todo ?
+});
+
+const props = defineProps<IconProps>();
+const filterProps = computed(() => omit(props, ["type", "color"]));
+const customStyles = computed(() => ({ color: props.color ?? void 0 }));
+</script>
+
+<template>
+  <!-- todo: i标签 ？ -->
+  <i
+    class="er-icon"
+    :class="[`er-icon-${props.type}`]"
+    :style="customStyles"
+    v-bind="$attrs"
+  >
+    <font-awesome-icon v-bind="filterProps" />
+  </i>
+</template>
+
+<style scoped>
+@import "./style.css";
+</style>
